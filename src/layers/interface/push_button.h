476ff2d765bed8a486dc25fe6736ae50c69f9adf
VitorPereiraFontes <vitor.fontes002@gmail.com>
2025-02-22 11:37:37 -0300

    Adicionando arquivo de cabeçalho
    Adicionando arquivo de cabeçalho para o bloco de push_button da camada
    de interface.
    
    Declarando e documentando as seguintes funções:
    
    setup_push_button
    read_push_button_state

---
+++ src/layers/interface/push_button.h
@@ -0,0 +1,29 @@
+/**
+ * @file push_button.h
+ * @author Vítor Pereira Fontes
+ * @date 2025-02-22
+ * 
+ * @brief Arquivo de cabeçalho para as funções que irão interagir diretamente com o push_button
+ */
+
+#ifndef _PUSH_BUTTON_H
+    #define _PUSH_BUTTON_H
+
+    #include "pico/stdlib.h"
+
+    /**
+     * @brief Inicializa e configura o push button conectado à uma GPIO
+     *
+     * @param gpio GPIO a qual o push button está conectado
+     */
+    void setup_push_button(uint8_t gpio);
+
+    /**
+     * @brief Verifica se o push button está pressionado
+     *
+     * @param gpio GPIO a qual o push button está conectado
+     * 
+     * @return Verdadeiro ou falso
+     */
+    bool read_push_button_state(uint8_t gpio);
+#endif
