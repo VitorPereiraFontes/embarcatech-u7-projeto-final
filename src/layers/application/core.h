99cd94953caf191a824d7bf1c8f3d2f638ca6db5
VitorPereiraFontes <vitor.fontes002@gmail.com>
2025-02-22 11:53:44 -0300

    Adicionando arqivo de cabeçalho
    Adicionando arquivo de cabeçalho para o bloco core da camada
    application.
    
    Definindo as GPIOS a serem utilizadas pela aplicação.
    
    Definindo frequência para comunicação com o display OLED.
    
    Definindo endereço I2C do display OLED.
    
    Definindo porta I2C para comunicação com o display OLED.

---
+++ src/layers/application/core.h
@@ -0,0 +1,24 @@
+/**
+ * @file core.h
+ * @author Vítor Pereira Fontes
+ * @date 2025-02-22
+ * 
+ * @brief Arquivo de cabeçalho para as funções principais da aplicação, que irão controlar efetivamente os 
+ * demais componentes. Abarca a definição das GPIO's e configurações utilizadas para cada componente.
+ */
+
+#ifndef _APPLICATION_H
+    #define _APPLICATION_H
+
+    #define PUSH_BUTTON_A_GPIO 5
+    #define PUSH_BUTTON_B_GPIO 6
+
+    #define I2C_PORT i2c1 // Porta utilizada para comunicação I2C com o display OLED
+    #define OLED_DISPLAY_I2C_ADDRESS 0x3C
+    #define OLED_DISPLAY_SDA_GPIO 14
+    #define OLED_DISPLAY_SCL_GPIO 15
+    #define OLED_DISPLAY_CLOCK_FREQUENCY 400 * 1000 // Define a frequência de comunicação com o  display OLED para 400 KHz
+
+    #define X_axis_laser_GPIO 21
+    #define Y_axis_laser_GPIO 22
+#endif
