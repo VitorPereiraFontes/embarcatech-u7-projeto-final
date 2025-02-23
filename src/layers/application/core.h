a398661d2d970fb55d10e8937ad795fa6b6d10f3
VitorPereiraFontes <vitor.fontes002@gmail.com>
2025-02-23 14:59:37 -0300

    Atualizando arquivos da camada 'Application'
    Removendo arquivo 'application.h'
    
    Removendo diretivas define do arquivo 'core.h'

--- src/layers/application/core.h
+++ src/layers/application/core.h
@@ -9,10 +9,4 @@
 
 #ifndef _APPLICATION_H
     #define _APPLICATION_H
-
-    #define I2C_PORT i2c1 // Porta utilizada para comunicação I2C com o display OLED
-    #define OLED_DISPLAY_I2C_ADDRESS 0x3C
-    #define OLED_DISPLAY_SDA_GPIO 14
-    #define OLED_DISPLAY_SCL_GPIO 15
-    #define OLED_DISPLAY_CLOCK_FREQUENCY 400 * 1000 // Define a frequência de comunicação com o  display OLED para 400 KHz
 #endif
