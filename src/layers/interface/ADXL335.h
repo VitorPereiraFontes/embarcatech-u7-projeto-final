0e66170b757b80a0998b34b95caf8a040ff50c78
VitorPereiraFontes <vitor.fontes002@gmail.com>
2025-02-24 22:31:54 -0300

    Atualizando arquivo de cabeçalho do módulo 'ADXL335'
    Incluindo diretivas define para armazenar valores referentes ao sensor.
    
    Declarando e documentando a função 'roud_to_2_decimal'.
    
    Atualizando o tipo de retorno da função 'read_acceleration' para float.

--- src/layers/interface/ADXL335.h
+++ src/layers/interface/ADXL335.h
@@ -11,9 +11,16 @@
 
     #include "pico/stdlib.h"
 
-    #define X_axis_GPIO 26
-    #define Y_axis_GPIO 27
-    #define Z_axis_GPIO 28
+    #define X_AXIS_GPIO 27
+    #define Y_AXIS_GPIO 26
+    #define Z_AXIS_GPIO 28
+    #define TYPICAL_SENSITIVITY 0.330f // Sensibilidade do sensor
+    #define ZERO_G_VOLTAGE 1.5f // Tensão do sensor em repouso, para os eixos X e Y, numa superfície completamente plana
+    #define XY_ZERO_G_OFFSET 0.15f // Valor da variação que o sensor considera como em repouso para os eixos X, Y e Z
+    #define XY_MAX_VOLTAGE 1.98f // Tensão máxima que o sensor consegue medir, para os eixos X e Y, em uma variação de +- 1g
+    #define XY_MIN_VOLTAGE 1.02f // Tensão mínima que o sensor consegue medir, para os eixos X e Y, em uma variação de +- 1g
+    #define Z_ZERO_G_OFFSET 0.3f // Valor da variação que o sensor considera como em repouso para o eixo Z
+    #define ADC_MAX_VALUE 4095.f
 
     /*
      * Enumeração que representa as entradas no multiplexador do ADC do RP2040.
@@ -27,6 +34,15 @@
     } ADC;
 
     /**
+     * @brief Arredonda a parte decimal de um número em ponto flutuante para que hajam apenas duas casas decimais
+     *
+     * @param value Valor que será arredondado
+     *
+     * @return Valor com duas casas decimais
+     */
+    float roud_to_2_decimal(float value);
+
+    /**
      * @brief Lê a aceleração da gravidade em um eixo escolhido.
      *
      * Esta função lê o valor de tensão de uma das saídas do módulo ADXL335 através do ADC da Raspberry Pi Pico W.
@@ -35,7 +51,7 @@
      *
      * @return Aceleração da gravidade no eixo escolhido.
      */
-    uint16_t read_acceleration(ADC adc);
+    float read_acceleration(ADC adc);
 
     /**
      * @brief Configura e inicializa o módulo
