f29d6b8a13b49e775c4d8ecc468793c07f64e612
VitorPereiraFontes <vitor.fontes002@gmail.com>
2025-02-24 22:51:49 -0300

    Integrando as funcionalidades no arquivo main.c
    Importando os seguintes arquivos de cabeçalho:
    
     - Core
     - Button
     - ADXL335
     - Push Button
     - KY008
     - SSD1306
    
    Adicionando as chamadas para as funções de inicialização e configuração
    dos seguintes módulos fora do loop:
    
     - ADXL335
     - KY008
     - Push Button
     - SSD1306
    
    Configurando o evento de interrupção e o callback para os botões.
    
    Adicionando as chamadas para as funções de execução dos seguintes
    módulos no loop:
    
     - KY008
     - ADXL335
     - Core
     - SSD1306
    
    Alterando o intervalo de sleep de 1000ms para 800ms

--- src/main.c
+++ src/main.c
@@ -1,11 +1,28 @@
 #include <stdio.h>
 #include "pico/stdlib.h"
+#include "layers/application/core.h"
+#include "layers/presentation/button.h"
+#include "layers/interface/ADXL335.h"
+#include "layers/interface/push_button.h"
+#include "layers/interface/KY008.h"
+#include "layers/interface/SSD1306.h"
 
 int main(){
     stdio_init_all();
 
+    initialize_push_button_block();
+
+    gpio_set_irq_enabled_with_callback(PUSH_BUTTON_A_GPIO, GPIO_IRQ_EDGE_FALL,true,interruption_callback);
+
+    setup_KY008_block();
+    setup_SSD1306_block();
+    setup_ADXL335_module();
+
     while (true) {
-        printf("Hello, world!\n");
-        sleep_ms(1000);
+        run_KY008_block();
+        run_ADXL335_module();
+        process_ADXL335_data();
+        run_SSD1306_block();
+        sleep_ms(800);
     }
 }
